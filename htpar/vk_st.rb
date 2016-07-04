bca5a7289fd7ef17f40cbfe0d3d2180cae1300fb
Vi Tak <trlearning@mail.ru>
2016-07-05 00:25:49 +0300

    puts dvf

--- htpar/vk_st.rb
+++ htpar/vk_st.rb
@@ -11,6 +11,7 @@ response = HTTParty.get('https://api.vk.com/method/users.get?user_ids=15553360&f
 response = HTTParty.get('https://api.vk.com/method/groups.getBanned?group_id=37273781&count=20&access_token=75afb43543589c98534b')
 response = HTTParty.get('https://api.vk.com/method/groups.getBanned?group_id=37273781&count=20&access_token=75afb43543dsdfsaf4b')
 
+puts "f wef wef'"
 body = response.body
 outp 'code', response.code
 outp 'msg', response.message
