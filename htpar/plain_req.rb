1275d16c1cb0d0986b5f0ed78653073c6b5fb5aa
Vi Tak <trlearning@mail.ru>
2016-07-04 21:45:07 +0300

    Add Gemfile, httparty usage examples

---
+++ htpar/plain_req.rb
@@ -0,0 +1,49 @@
+require 'httparty'
+require 'json'
+
+response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=askubuntu')
+
+walk_by_reps(JSON.parse(response.body)["items"]).each(&:join)
+
+BEGIN {
+	def outp(title, body)
+		puts "-" * 30
+		puts "---#{title}---"
+		puts "#{body}"
+		puts "-" * 30
+	end
+
+	def do_desc(option, value)
+		puts "#{option}:  #{value}"
+	end
+
+	def walk_by_reps(items)
+		threads = []
+		items.each_with_index do |item, i|
+			threads << Thread.new(item, i) do |item, i|
+				puts "--#{i}---\n---"
+
+				walker = Walker.new(item)
+				walker.short_info
+			end
+		end
+		threads
+	end
+
+	class Walker
+		attr_reader :item
+
+		def initialize(item)
+			@item = item
+		end
+
+		def outp
+			short_info
+		end
+
+		def short_info
+			do_desc 'reput', item["owner"]["reputation"]
+			do_desc 'd_name', item["owner"]["display_name"]
+		end
+	end
+}
