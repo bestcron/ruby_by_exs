ad28b196c5645db5a110a640fe0da601a5e492d9
Vi Tak <trlearning@mail.ru>
2016-07-05 00:34:39 +0300

    Af efvewfwe

--- htpar/vk_st.rb
+++ htpar/vk_st.rb
@@ -12,6 +12,9 @@ response = HTTParty.get('https://api.vk.com/method/groups.getBanned?group_id=372
 response = HTTParty.get('https://api.vk.com/method/groups.getBanned?group_id=37273781&count=20&access_token=75afb43543dsdfsaf4b')
 
 puts "f wef wef'"
+f = 434 
+puts "f: #{f}"
+
 body = response.body
 outp 'code', response.code
 outp 'msg', response.message
